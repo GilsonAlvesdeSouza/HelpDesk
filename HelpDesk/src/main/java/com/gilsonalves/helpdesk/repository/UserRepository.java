3f399c6884ffd1c26648d635b10e3f87c81ce80f
Gilson Alves <gilsonalves6@icloud.com>
2019-07-18 19:40:26 -0300

    teste

--- HelpDesk/src/main/java/com/gilsonalves/helpdesk/repository/UserRepository.java
+++ HelpDesk/src/main/java/com/gilsonalves/helpdesk/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.gilsonalves.helpdesk.repository;
 
-public interface UserRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String>{
 
 }
